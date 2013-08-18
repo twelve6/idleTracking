c63e4314ee060d6184e436fd93d8870dff2bc370
Luis Ramirez <luis@twelve6.com>
2013-08-17 22:17:41 -0500

    init plugin commit, updated readme

--- README.md
+++ README.md
@@ -1,4 +1,75 @@
 idleTracking
-============
+===========
 
-Simple jQuery plugin to track an idle user.
+**Idle User tracking & Follow up Action**
+
+Simple jQuery plugin to track time difference between the last active time stamp and current time to determine an idle user. If you need a solution that will work on devices that suspend the browser (e.g iOS) or can't rely on a user being logged out by the server on browser load, then hopefully my plugin will work for you. This plugin is modeled after Paul Irish's idleTimer and Eric Hynd's idleTracking and can coexist and work together for a bullet proof solution.
+
+**Give Me Your Feedback**
+
+I want to hear from you, where can we improve this plugin?
+
+**Demo Page Coming soon**
+
+sorry!
+
+**Usage**
+
+A user is considered active with the following events
+
+mousemove, keydown, DOMMouseScroll, mousewheel, mousedown, touchstart, touchmove
+
+Since localStorage support is pretty good
+we store a date and time stamp and update them on the events above
+
+* http://www.html5rocks.com/en/features/storage
+
+As a fall back we also use data attributes
+
+```javascript
+//Call defaults to 15 min timeout & redirect to /logout
+$.idleTracking();
+
+//Set timeout 20 min
+$.idleTracking({ timeout : 20 });
+
+//Set window location on timeout
+$.idleTracking({ logout : 'index.php' });
+
+//or Call your own function on idle
+$.idleTracking({
+    onIdle : function() {
+        //fire when idle
+        //You can call the logout method too
+        this.logout();
+    }
+});
+
+//Clean up the timestamp
+//Using the internal logout method will clean up after itself
+//but if you don't use it you need to clean up
+//removes localStorage and data attributes
+$.idleTracking.clean();
+
+//You can also call the logout method
+//remember to set your logout : 'options'
+$.idleTracking.logout();
+
+//This one's in case you need to reset the time stamp
+$.idleTracking.reset();
+
+});
+```
+
+**Potential Dangers**
+
+* Data attributes are refreshed on reload, so be careful relying on this fall back.
+
+*Example*
+
+* iOS full screen web apps refresh every time they're launched.  Thankfully we can rely on localStorage to store when the user was active last and on load determine if we need to log out the user or any other action.
+
+
+
+
+
---
+++ jquery.idletracking.js
@@ -0,0 +1,162 @@
+/*
+ * jQuery idleTracking 1.0
+ * 2013 Luis Ramirez
+ *
+ * Depends:
+ *  - jQuery 1.4.2+
+ * Plays Well with:
+ *  - jQuery Idle Timer (by Paul Irish, https://github.com/mikesherov/jquery-idletimer)
+ *  - jQuery Idle Timeout (by Eric Hynds, http://www.erichynds.com/jquery/a-new-and-improved-jquery-idle-timeout-plugin/)
+ *
+ *
+*/
+;(function($, undefined){
+
+    var idleTracking = {
+
+        _localStorage : false,
+
+        defaults : {
+            timeout : 15,
+            logout : '/logout',
+            onReset: $.noop,
+            onIdle: $.noop
+        },
+
+        settings :  {},
+
+        init    :   function(opt) {
+            var self = idleTracking;
+            self.settings = $.extend({}, self.defaults, opt);
+            //check for localStorage support
+            self._localStorage = self._localStorageTest();
+            //attach our events
+            self._events();
+            //Check for idle user
+            self._checkIdleUser();
+            //expose our object
+            // $.data(document, 'idleTracking', self);
+        },
+
+        _localStorageTest   :   function() {
+            var self = idleTracking;
+            try {
+                localStorage.setItem('mod', 'mod');
+                localStorage.removeItem('mod');
+                return true;
+            } catch(e) {
+                return false;
+            }
+        },
+
+        _checkIdleUser  :   function() {
+            var self = idleTracking;
+            var obj = {};
+
+            if (self._localStorage) {
+                obj = self._getDataStorage();
+            } else {
+                obj = self._getDataAttr();
+            }
+
+            if (obj.timeStamp !== null && obj.dateStamp !== null) {
+
+                var newDate = new Date();
+                var date = (newDate.getUTCMonth() + 1) + '' + newDate.getUTCDate() + '' + newDate.getUTCFullYear();
+                var time = newDate.getTime();
+
+                if(date !== obj.dateStamp || time - obj.timeStamp > (self.settings.timeout * 60) * 1000) {
+                    self.settings.onIdle.call(self);
+                }
+            }
+            setTimeout(self._checkIdleUser, 1000);
+        },
+
+        _getDataStorage     :   function() {
+            var obj = {};
+            obj['dateStamp'] = localStorage.getItem('idleDate');
+            obj['timeStamp'] = localStorage.getItem('idleTime');
+            return obj;
+        },
+
+        _getDataAttr    :   function() {
+            obj = {};
+            obj['dateStamp'] = $(document).data('idleDate');
+            obj['timeStamp'] = $(document).data('idleTime');
+            return obj;
+        },
+
+        _events     :   function() {
+        var self = idleTracking;
+            //assign appropriate event handlers
+            var events  = 'mousemove keydown DOMMouseScroll mousewheel mousedown touchstart touchmove';
+            $(document).bind(events, function() {
+                self._timeStamp();
+            });
+        },
+
+        _timeStamp :    function() {
+            var self = idleTracking;
+            var newDate = new Date();
+            var date = (newDate.getUTCMonth() + 1) + '' + newDate.getUTCDate() + '' + newDate.getUTCFullYear();
+            var time = newDate.getTime();
+
+            if (self._localStorage) {
+                localStorage.setItem('idleDate', date);
+                localStorage.setItem('idleTime', time);
+            }
+
+            $(document).data('idleTime', time);
+            $(document).data('idleDate', date);
+        },
+
+        houseKeeping   :   function() {
+            var self = idleTracking;
+            //we might need to call this method, before init
+            //so we need to check support
+            self._localStorage = self._localStorageTest();
+
+            //we have to remove idleTime otherwise it will persist
+            if (self._localStorage) {
+                localStorage.removeItem('idleTime');
+                localStorage.removeItem('idleDate');
+            }
+            $(document).removeData('idleTime');
+            $(document).removeData('idleDate');
+        },
+
+        logout :    function() {
+            var self = idleTracking;
+            self.houseKeeping();
+            window.location = self.settings.logout;
+        },
+
+        reset   :   function() {
+            var self = idleTracking;
+            self._timeStamp();
+            self.settings.onReset.call(this);
+        }
+    };
+
+    //expose
+    $.idleTracking = function(opt) {
+        idleTracking.init(opt);
+        return this;
+    };
+    //clean house
+    $.idleTracking.clean = function() {
+        idleTracking.houseKeeping();
+        return this;
+    };
+    //expose our logout
+    $.idleTracking.logout = function() {
+        idleTracking.logout();
+        return this;
+    };
+    //expose our reset
+    $.idleTracking.reset = function() {
+        idleTracking.reset();
+        return this;
+    };
+
+})(jQuery);//end
